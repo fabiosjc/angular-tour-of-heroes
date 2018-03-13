f20a2e8d5d5f9084ed207205833a9d635a814e7e
Fábio Henrique <fabiosjc@gmail.com>
2018-03-12 23:35:52 -0300

    04 - Master/Detail Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDertailComponent } from './hero-dertail/hero-dertail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDertailComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
